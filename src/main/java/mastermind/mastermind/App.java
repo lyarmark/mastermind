6f03a462eab4fb6fceb4176006071c624b6e399f
AriellaFeuchtwanger <acfeuchtwanger@gmail.com>
2016-02-05 14:45:30 -0500

    Initial commit

---
+++ src/main/java/mastermind/mastermind/App.java
@@ -0,0 +1,13 @@
+package mastermind.mastermind;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
