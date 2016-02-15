fa191f9dc5b1bb5a57dfde9247af231038c5cb30
lyarmark <lyarmark@student.touro.edu>
2016-02-14 20:47:15 -0500

    merge

--- src/main/java/mastermind/Board.java
+++ src/main/java/mastermind/Board.java
@@ -2,8 +2,6 @@ package mastermind;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
